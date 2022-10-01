96ed64a1c14c73d93e75a18f49b063912b8ad754
dnicholson0011 <81931232+dnicholson0011@users.noreply.github.com>
2022-10-01 18:43:21 -0500

    Add

--- src/main/java/com/wewanttech/employeemanager/service/EmployeeService.java
+++ src/main/java/com/wewanttech/employeemanager/service/EmployeeService.java
@@ -1,4 +1,29 @@
 package com.wewanttech.employeemanager.service;
 
+import com.wewanttech.employeemanager.model.Employee;
+import com.wewanttech.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class EmployeeService {
+
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
 }
